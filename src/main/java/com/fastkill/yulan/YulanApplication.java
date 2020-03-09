07efcd327e0fb0120d5f488bc4aeb847d3335a30
YuLan-account <1748575538@qq.com>
2020-03-09 21:33:50 +0800

    test for commit!

---
+++ src/main/java/com/fastkill/yulan/YulanApplication.java
@@ -0,0 +1,13 @@
+package com.fastkill.yulan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YulanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YulanApplication.class, args);
+    }
+
+}
